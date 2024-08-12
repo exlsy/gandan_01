e03b68ea668399a1c87c4ba8cbd9e110ba9d7151
exlsy <exsylee@gmail.com>
2024-08-12 23:19:33 +0900

    calendar

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -17,6 +17,30 @@ class Calendar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final _events = LinkedHashMap(
+    //   equals: isSameDay,
+    // )..addAll({
+    //     DateTime(2024, 8, 4):
+    //         Event(date: DateTime(2024, 8, 4), fastingHour: 10),
+    //     DateTime(2024, 8, 6):
+    //         Event(date: DateTime(2024, 8, 6), fastingHour: 14),
+    //     DateTime(2024, 8, 7):
+    //         Event(date: DateTime(2024, 8, 7), fastingHour: 16),
+    //     DateTime(2024, 8, 9):
+    //         Event(date: DateTime(2024, 8, 9), fastingHour: 18),
+    //   });
+    Map<DateTime, Event> events = {
+      DateTime(2024, 8, 1): Event('6h'),
+      DateTime(2024, 8, 2): Event('24h'),
+      DateTime(2024, 8, 3): Event('9h'),
+      DateTime(2024, 8, 5): Event('18h'),
+      DateTime(2024, 8, 12): Event('16h'),
+    };
+
+    Event _getEventsForDay(DateTime day) {
+      return events[day] ?? Event('');
+    }
+
     final defaultBoxDecoration = BoxDecoration(
       // color: Colors.lightBlue,
       borderRadius: BorderRadius.circular(6.0),
@@ -64,6 +88,46 @@ class Calendar extends StatelessWidget {
       ),
       onDaySelected: onDaySelected,
       selectedDayPredicate: selectedDayPredicate,
+      calendarBuilders: CalendarBuilders(
+        markerBuilder: (context, date, events) {
+          DateTime _date = DateTime(date.year, date.month, date.day);
+          print("${_date} --> ${_getEventsForDay(_date).title}");
+          if (_getEventsForDay(_date).title.length > 0) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  // color: Colors.blue,
+                  width: 30,
+                  height: 15,
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    // borderRadius: BorderRadius.circular(12.0),
+                    border: Border.all(
+                      color: Colors.grey[200]!,
+                      width: 1.0,
+                    ),
+                  ),
+
+                  child: Text(
+                    '${_getEventsForDay(_date).title}',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
     );
   }
+}
+
+class Event {
+  String title;
+  Event(this.title);
 }
